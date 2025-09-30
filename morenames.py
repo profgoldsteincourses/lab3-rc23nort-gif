fa6c5a20a5d35e2e8b632e8ef6428d28a7edc4eb
NazirD-ai <nh14deme@siena.edu>
2025-09-30 11:08:45 -0400

    Added new file morenames.py, which print first and last names

---
+++ morenames.py
@@ -0,0 +1,14 @@
+# names.py
+# CSIS-110 Fall 2025
+# Ira Goldstein
+
+# Simple loop using range()
+
+# A function to print a series of names.
+# It does not have any parameters
+def printNames():
+  for myName in ["Drake","Taylor","Billy"]:
+        for lastName in ["Graham","Swift","Bob" ]:
+             print (myName + " " + lastName)
+# Call our function
+printNames()
